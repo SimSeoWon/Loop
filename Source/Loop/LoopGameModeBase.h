8cfa52fcf59b061c803a72e3f60e474855c79b5a
SimSeoWon <30568331+SimSeoWon@users.noreply.github.com>
2023-02-11 23:44:49 +0900

    소스 파일 추가

---
+++ Source/Loop/LoopGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoopGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOOP_API ALoopGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
